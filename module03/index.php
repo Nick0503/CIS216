672ac1bd6b64d6fd9e69dc186190a1127bff530e
Nick0503 <89652576+Nick0503@users.noreply.github.com>
2021-11-09 16:03:59 -0600

    Update index.php

--- module03/index.php
+++ module03/index.php
@@ -1,3 +1,25 @@
 <title> Java Jam Coffee Point </title>
-
-<h1>Coffee at Java Jam Coffee Point</h1>
+<br>
+<a href="index.html">Home</a>
+<a href="menu.html">Menu</a>
+<a href="music.html">Music</a>
+<a href="jobs.html">Home</a>
+<br>
+<h2> Relax at Java <h2>
+<ul> 
+  <li> Specailty Coffee and Tea <li>
+    <li> Bagels, Muffins, and Gluten Free Pastries <li>
+    <li> Organic Salads <li>
+    <li> Music and Poetry Readings <li>
+    <li> Open Mic Night <li>
+  <ul>
+    <br>
+    <div> 12010 Garret Bay Road 
+      <br>
+      Ellison Bay, WI 54210
+      <br>
+      888-555-5555
+      <div>
+      <small><i> Copyright &copy 2018 JavaJam Coffee House <br>
+        <a href="Nicholas@Ledesma.com </a>
+                 
